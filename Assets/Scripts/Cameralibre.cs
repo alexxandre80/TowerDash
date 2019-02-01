87d2cbe4d01123295732b7c05e5cae0edc9ddc2a
Alexandre <mandra80080@gmail.com>
2019-02-01 13:08:00 +0100

    Alpha V0.1 Unity
    Alpha V0.1 Unity

---
+++ Assets/Scripts/Cameralibre.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Cameralibre : MonoBehaviour
+{
+    public Transform lookAt;
+        
+    private float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensivityX = 0.0f;
+    private float sensivityY = 0.0f;
+
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+    
+    private void LateUpdate()
+    {
+        Vector3 direction = new Vector3(0,0,-distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        transform.position = lookAt.position + rotation * direction;
+
+    }
+}
---
+++ Assets/Scripts/MoveCubeScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCubeScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetTransform;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            targetTransform.position +=
+                Vector3.back * Time.deltaTime * moveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            targetTransform.position +=
+                Vector3.forward * Time.deltaTime * moveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            targetTransform.position +=
+                Vector3.left * Time.deltaTime * moveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            targetTransform.position +=
+                Vector3.right * Time.deltaTime * moveSpeed;
+        }
+    }
+}
